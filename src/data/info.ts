dcb5a7edf1d0976e67606e76b71da9dbf9183f59
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-24 14:27:36 +0100

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,115 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Oladele John",
+  jobDescription: "Web Developer",
+  about: `I'm a Web Developer,
+   That enjoys creating things that live on the internet, focused on building dynamic web specific applications.
+   Skilled in HTML, CSS, JavaScript, Bootstrap, tailwind CSS, React JS, Next JS. Hence,
+   I have successfully completed several web projects, and also gained real world experience while working as web developer which includes collaborating with co-developers. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I'm also gaining knowledge of VueJs and Nuxt.
+  `,
+/*
+
+I’m A Passionate Web Developer, focused on building dynamic web specific applications, turning deadlines into deliveries, 
+problems into solutions, bugs into features, constantly learning new stuff and doing my best in making the world a better 
+place.
+
+*/ 
+  experience: [
+    {
+      name: "InterNative Labs, LLC",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Coupon Approve (Remote)",
+      location: "Amsterdam, North Holland, Netherlands",
+      startDate: "Nov 2020",
+      endDate: "Apr 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Interns Pakistan",
+      location: "Pakistan",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2019",
+      endDate: "2022",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
+    },
+    {
+      name: "Govt. Degree Boys College Buffer Zone",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["Intermediate, Pre-Engineering"],
+    },
+    {
+      name: "Ghazi Foundation Schools & Colleges",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Matriculation,  Computer Science"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "Tasued-Cbt-Practice",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Tasued-Cbt-Practice",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Tasued-Cbt-Practice",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
