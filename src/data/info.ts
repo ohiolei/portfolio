2d1c881699e77d31b6629503495c06f0b0876fb1
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-24 17:50:47 +0100

    Updated projects

--- src/data/info.ts
+++ src/data/info.ts
@@ -16,77 +16,12 @@ problems into solutions, bugs into features, constantly learning new stuff and d
 place.
 
 */ 
-  experience: [
-    {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
-      description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
-      ],
-    },
-  ],
-
-  education: [
-    {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
-    },
-  ],
+  
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    twitter: "https://twitter.com/coding_wizard",
+    github: "https://github.com/jhon-crypt",
+    email: "johnoladele690@74372gmail.com",
   },
 
   projects: [
@@ -94,22 +29,36 @@ place.
       title: "Tasued-Cbt-Practice",
       isFeatured: true,
       thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      githubUrl: "https://github.com/Jhon-crypt/tasued-cbt-practice",
       liveUrl: "https://astro-portfolio-uzair.vercel.app/",
     },
     {
-      title: "Tasued-Cbt-Practice",
+      title: "To Be Very Honest",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889742/portfolio/tbvh_khfsma.jpg",
+      githubUrl: "https://github.com/Jhon-crypt/tbvh",
       liveUrl: "https://astro-portfolio-uzair.vercel.app/",
     },
     {
-      title: "Tasued-Cbt-Practice",
+      title: "Yofte Ecommerce Landing Page",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889857/portfolio/yofteVue_ntyzih.jpg",
+      githubUrl: "https://github.com/Jhon-crypt/yofteVue",
       liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Lifemachines Landing Page",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692891612/portfolio/lifemachines_gkmw6z.jpg",
+      githubUrl: "https://github.com/Jhon-crypt/life-machines",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Wizard Ai",
+      isFeatured: true,
+      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692892774/portfolio/wizard_gpfl2j.jpg",
+      githubUrl: "https://github.com/Jhon-crypt/wizardAi",
+      liveUrl: "https://wizardai.vercel.app/",
     },
   ],
 };
