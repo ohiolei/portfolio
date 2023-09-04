5d4c3488b83df268296e85c069bef7f173f0ca50
ohiolei <willnagato@gmail.com>
2023-09-04 15:15:01 +0100

    update

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,13 +1,14 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Oladele John",
+  name: "Williams Oluwatobiloba",
   jobDescription: "Web Developer",
-  about: `I'm a Web Developer,
-   That enjoys creating things that live on the internet, focused on building dynamic web specific applications.
-   Skilled in HTML, CSS, JavaScript, Bootstrap, tailwind CSS, React JS, Next JS. Hence,
-   I have successfully completed several web projects, and also gained real world experience while working as web developer which includes collaborating with co-developers. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I'm also gaining knowledge of VueJs and Nuxt.
+  about: `I'm a fullstack Web Developer,
+  that possess a wide range of abilities which combines innovative art, design principles, optimization of
+  website performance, investigation and troubleshooting issues ensuring smoother processes for
+  every end user.
+  Furthermore, my expert knowledge of Adobe Creative Suite, HTML, CSS, Bootstrap, Tailwind, C#, PHP, Vue.Js, Laravael and ASP.NET will
+  allow me to play a crucial role in the implementation of your design and ideas.
+  
   `,
 /*
 
@@ -19,53 +20,53 @@ place.
   
 
   socialMedia: {
-    twitter: "https://twitter.com/coding_wizard",
-    github: "https://github.com/jhon-crypt",
-    email: "johnoladele690@74372gmail.com",
+    twitter: "",
+    github: "https://github.com/ohiolei",
+    email: "willnagato@gmail.com",
   },
 
   projects: [
     {
-      title: "Oanwtech",
+      title: "Artisan Go",
       isFeatured: true,
       thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1693040953/oawn_dfdx6u.jpg",
-      githubUrl: "",
-      liveUrl: "https://oanwtech.com/",
+      githubUrl: "https://github.com/ohiolei/artisan_go_frontend",
+      liveUrl: "",
     },
     {
-      title: "Tasued-Cbt-Practice",
+      title: "Artisan Go API's",
       isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692876919/portfolio/tascreen_yea7jy.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/tasued-cbt-practice",
-      liveUrl: "https://tasued-cbt-practice.vercel.app/",
+      thumbnail: "",
+      githubUrl: "https://github.com/ohiolei/artisan_go_backend",
+      liveUrl: "",
     },
-    {
-      title: "To Be Very Honest",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889742/portfolio/tbvh_khfsma.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/tbvh",
-      liveUrl: "https://tbvh.vercel.app/",
-    },
-    {
-      title: "Yofte Ecommerce Landing Page",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889857/portfolio/yofteVue_ntyzih.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/yofteVue",
-      liveUrl: "https://https://yofte-vue.vercel.app.vercel.app/",
-    },
-    {
-      title: "Lifemachines Landing Page",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692891612/portfolio/lifemachines_gkmw6z.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/life-machines",
-      liveUrl: "https://life-machines.vercel.app/",
-    },
-    {
-      title: "Wizard Ai",
-      isFeatured: true,
-      thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692892774/portfolio/wizard_gpfl2j.jpg",
-      githubUrl: "https://github.com/Jhon-crypt/wizardAi",
-      liveUrl: "https://wizardai.vercel.app/",
-    },
+    // {
+    //   title: "To Be Very Honest",
+    //   isFeatured: true,
+    //   thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889742/portfolio/tbvh_khfsma.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/tbvh",
+    //   liveUrl: "https://tbvh.vercel.app/",
+    // },
+    // {
+    //   title: "Yofte Ecommerce Landing Page",
+    //   isFeatured: true,
+    //   thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692889857/portfolio/yofteVue_ntyzih.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/yofteVue",
+    //   liveUrl: "https://https://yofte-vue.vercel.app.vercel.app/",
+    // },
+    // {
+    //   title: "Lifemachines Landing Page",
+    //   isFeatured: true,
+    //   thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692891612/portfolio/lifemachines_gkmw6z.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/life-machines",
+    //   liveUrl: "https://life-machines.vercel.app/",
+    // },
+    // {
+    //   title: "Wizard Ai",
+    //   isFeatured: true,
+    //   thumbnail: "https://res.cloudinary.com/db7wwc9ex/image/upload/v1692892774/portfolio/wizard_gpfl2j.jpg",
+    //   githubUrl: "https://github.com/Jhon-crypt/wizardAi",
+    //   liveUrl: "https://wizardai.vercel.app/",
+    // },
   ],
 };
